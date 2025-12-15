8b8de5b37191f1cb9f9d08d24a74e2295916e81e
Jiho Bok <bok.jiho@gmail.com>
2025-12-15 21:08:45 +0000

    feat: implement item selection steps

--- src/pages/new-game/index.tsx
+++ src/pages/new-game/index.tsx
@@ -1,39 +1,94 @@
-import { useMemo, useState } from "react";
+import { type Items, type ItemKey, items, Item } from "@/game/items";
+import Image from "next/image";
+import { ReactElement, useEffect, useMemo, useState } from "react";
 
 type Props = {};
 type ItemsFormProps = {
   step: number;
 };
 
-function ItemsForm({ step }: ItemsFormProps) {
-  if (step === 1) {
-    return <div>step1</div>;
-  } else if (step === 2) {
-    return <div>step2</div>;
-  } else {
-    return <div>Selected items: </div>;
-  }
-}
+const MIN_LIKES_COUNT = 1;
+const MAX_LIKES_COUNT = 5;
+const MIN_DISLIKES_COUNT = 1;
+const MAX_DISLIKES_COUNT = 5;
 
 export default function NewGame({}: Props) {
   const totalSteps = 3;
   const [currentStep, setCurrentStep] = useState(1);
   const isLastStep = useMemo(() => currentStep === totalSteps, [currentStep]);
-  const [itemOptions, setItemOptions] = useState([]);
-  const [selectedLikes, setSelectedLikes] = useState([]);
-  const [selectedDislikes, setSelectedDislikes] = useState([]);
+  const [itemOptions, setItemOptions] = useState(items);
+  const [selectedLikes, setSelectedLikes] = useState<ItemKey[]>([]);
+  const [selectedDislikes, setSelectedDislikes] = useState<ItemKey[]>([]);
+  const dislikeOptions: Partial<Items> = useMemo(() => {
+    const entries = Object.entries(itemOptions).filter(
+      ([key, _]) => !selectedLikes.includes(key as ItemKey)
+    );
+    return Object.fromEntries(entries);
+  }, [selectedLikes, itemOptions]);
 
-  const handleNextClick = (val: number) => {
-    if (isLastStep && val > 0) {
+  const handlePrevClick = () => {
+    setCurrentStep((prev) => prev - 1);
+  };
+
+  const handleNextClick = () => {
+    if (isLastStep) {
       // @todo create game
       console.log("create game");
-    } else {
-      setCurrentStep((prev) => prev + val);
+      return;
+    }
+
+    if (
+      (currentStep === 1 && selectedLikes.length < 1) ||
+      (currentStep === 2 && selectedDislikes.length < 1)
+    ) {
+      console.log("Select at least 1 item");
+      return;
+    }
+
+    setCurrentStep((prev) => prev + 1);
+  };
+
+  const handleItemClick = (
+    e: React.MouseEvent<HTMLLIElement>,
+    itemKey: ItemKey
+  ) => {
+    if (currentStep === 1) {
+      if (selectedLikes.includes(itemKey)) {
+        setSelectedLikes((likedList) =>
+          likedList.filter((key) => key !== itemKey)
+        );
+      } else {
+        if (MAX_LIKES_COUNT === selectedLikes.length) {
+          console.log("Cannot select more! Reached maximum selections");
+          return;
+        }
+        setSelectedLikes((likedList) => [...likedList, itemKey]);
+      }
+    } else if (currentStep === 2) {
+      if (selectedDislikes.includes(itemKey)) {
+        setSelectedDislikes((dislikedList) =>
+          dislikedList.filter((key) => key !== itemKey)
+        );
+      } else {
+        if (MAX_DISLIKES_COUNT === selectedDislikes.length) {
+          console.log("Cannot select more! Reached maximum selections");
+          return;
+        }
+        setSelectedDislikes((dislikedList) => [...dislikedList, itemKey]);
+      }
+    }
+  };
+
+  const isSelected = (key: ItemKey) => {
+    if (currentStep === 1) {
+      return selectedLikes.includes(key);
+    } else if (currentStep === 2) {
+      return selectedDislikes.includes(key);
     }
   };
 
   return (
-    <section className="flex flex-col justify-center gap-5 w-[500px] h-dvh mx-auto ">
+    <section className="flex flex-col justify-center gap-5 w-[500px] max-w-dvw h-auto p-5 mx-auto">
       <h1>
         If Santa were to get you presents, what would you like or dislike for
         Christmas?
@@ -42,24 +97,121 @@ export default function NewGame({}: Props) {
         Step {currentStep} / {totalSteps}
       </h2>
 
-      <div>
-        <ItemsForm step={currentStep} />
+      <div className={currentStep === 1 ? "block" : "hidden"}>
+        <div>I would like.. {selectedLikes.length} selected</div>
+        <p>
+          Select {MIN_LIKES_COUNT} to {MAX_LIKES_COUNT} items
+        </p>
+        <ul className="grid grid-cols-4 md:grid-cols-5 gap-2 md:gap-3">
+          {Object.entries(itemOptions).map(([itemKey, itemData]) => {
+            return (
+              <li
+                className={`col text-center rounded-xl p-2 bg-green-100 text-sm text-black hover:cursor-pointer ${
+                  isSelected(itemKey as ItemKey) &&
+                  " bg-green-600 font-semibold text-white"
+                }`}
+                key={itemKey as ItemKey}
+                onClick={(e) => handleItemClick(e, itemKey as ItemKey)}
+              >
+                <Image
+                  className="mx-auto"
+                  src={itemData.path}
+                  aria-label={itemData.name}
+                  alt={itemData.name}
+                  width={30}
+                  height={30}
+                />
+                {itemData.name}
+              </li>
+            );
+          })}
+        </ul>
       </div>
 
-      <button
-        className="p-3 rounded-xl bg-green-700 hover:cursor-pointer disabled:cursor-not-allowed disabled:bg-green-900"
-        disabled={currentStep <= 1}
-        onClick={() => handleNextClick(-1)}
-      >
-        Prev
-      </button>
+      <div className={currentStep === 2 ? "block" : "hidden"}>
+        <div>I would NOT like.. {selectedDislikes.length} selected</div>
+        <p>
+          Select {MIN_DISLIKES_COUNT} to {MAX_DISLIKES_COUNT} items
+        </p>
 
-      <button
-        className="p-3 rounded-xl bg-green-700 hover:cursor-pointer"
-        onClick={() => handleNextClick(1)}
-      >
-        {isLastStep ? "Create game" : "Next"}
-      </button>
+        <ul className="grid grid-cols-4 md:grid-cols-5 gap-2 md:gap-3">
+          {Object.entries(dislikeOptions).map(([itemKey, itemData]) => {
+            return (
+              <li
+                className={`col text-center rounded-xl p-2 bg-green-100 text-sm text-black hover:cursor-pointer ${
+                  isSelected(itemKey as ItemKey) &&
+                  " bg-green-600 font-semibold text-white"
+                }`}
+                key={itemKey}
+                onClick={(e) => handleItemClick(e, itemKey as ItemKey)}
+              >
+                <Image
+                  className="mx-auto"
+                  src={itemData.path}
+                  aria-label={itemData.name}
+                  alt={itemData.name}
+                  width={30}
+                  height={30}
+                />
+                {itemData.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className={`${currentStep === 3 ? "block" : "hidden"}`}>
+        <h3>Selected items: </h3>
+        <div className="flex flex-col gap-2">
+          <p>Likes: {selectedLikes.length} items selected</p>
+          <ul className="flex gap-2">
+            {selectedLikes.map((key) => (
+              <li className="p-2 rounded-2xl text-sm bg-green-100 text-black">
+                <Image
+                  src={itemOptions[key].path}
+                  width={30}
+                  height={30}
+                  alt={itemOptions[key].name}
+                />
+                {itemOptions[key].name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p>Dislikes: {selectedDislikes.length} items selected</p>
+          <ul className="flex gap-2">
+            {selectedDislikes.map((key) => (
+              <li className="p-2 rounded-2xl text-sm bg-green-100 text-black">
+                <Image
+                  src={itemOptions[key].path}
+                  width={30}
+                  height={30}
+                  alt={itemOptions[key].name}
+                />
+                {itemOptions[key].name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="flex gap-2">
+        <button
+          className="flex-1/2 p-3 rounded-xl bg-green-700 hover:cursor-pointer disabled:cursor-not-allowed disabled:bg-green-900"
+          disabled={currentStep <= 1}
+          onClick={handlePrevClick}
+        >
+          Prev
+        </button>
+
+        <button
+          className="flex-1/2 p-3 rounded-xl bg-green-700 hover:cursor-pointer"
+          onClick={handleNextClick}
+        >
+          {isLastStep ? "Create game" : "Next"}
+        </button>
+      </div>
     </section>
   );
 }
